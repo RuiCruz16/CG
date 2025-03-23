6dd284865a21ea8a88a7020358d124212fed265a
Rui <up202208011@edu.fe.up.pt>
2025-03-23 18:57:00 +0000

    TP4 ex2 done

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -7,45 +7,69 @@ import { MyQuad } from "./MyQuad.js";
  * @param scene - Reference to MyScene object
  */
 export class MyUnitCubeQuad extends CGFobject {
-    constructor(scene) {
+    constructor(scene, textureTop, textureFront, textureRight, textureBack, textureLeft, textureBottom) {
         super(scene);
         this.quad = new MyQuad(scene);
+        this.textureTop = textureTop;
+        this.textureFront = textureFront;
+        this.textureRight = textureRight;
+        this.textureBack = textureBack;
+        this.textureLeft = textureLeft;
+        this.textureBottom = textureBottom;
     }
 
     display() {
       
+        // Front
         this.scene.pushMatrix();
             this.scene.translate(0,0,0.5);
+            this.textureFront.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
+        // Right Side
         this.scene.pushMatrix();
             this.scene.rotate(Math.PI/2, 0,1,0);
             this.scene.translate(0,0,0.5);
+            this.textureRight.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
+        // Left Side
         this.scene.pushMatrix();
             this.scene.rotate(-Math.PI/2, 0,1,0);
             this.scene.translate(0,0,0.5);
+            this.textureLeft.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
+        // Top
         this.scene.pushMatrix();
             this.scene.rotate(-Math.PI/2, 1,0,0);
             this.scene.translate(0,0,0.5);
+            this.textureTop.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
+        // Back
         this.scene.pushMatrix();
-            this.scene.rotate(-Math.PI, 1,0,0);
+            this.scene.rotate(Math.PI, 0,1,0);
             this.scene.translate(0,0,0.5);
+            this.textureBack.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
+        // Bottom
         this.scene.pushMatrix();
             this.scene.rotate(Math.PI/2, 1,0,0);
             this.scene.translate(0,0,0.5);
+            this.textureBottom.bind();
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
             this.quad.display();
         this.scene.popMatrix();
 
---
+++ tp4/README.md
@@ -0,0 +1,19 @@
+# CG 2024/2025
+
+## Group T13G08
+
+## TP 4 Notes
+
+In this TP, the aim was to define the coordinates of a texture and combine it with the use of materials to obtain realistic appearances.
+
+- Through our initial experiments, we explored the different ways of applying texture, where we mainly had initial difficulties understanding the ‘Clamp to Edge’ wrap mode, but after a few tests it became clear.
+
+- In the first exercise, we were asked to apply a texture to each of the Tangram pieces using a given image. We had to load the texture correctly and pay close attention to the texture coordinates for each piece. The only piece that presented some difficulty was the parallelogram, as its orientation in the image was reversed compared to the initial position we had defined in the Tangram.
+
+- In the second exercise, the objective was well understood and there were no major difficulties. However, at first, when we rendered the cube, all of its faces appeared with blue tones. These tones were later corrected by the expected colors when we displayed the Quad.
+
+![Screenshot 1](screenshots/CG-t13g08-tp4-1.png)
+*Figure 1: TP4-1*
+
+![Screenshot 2](screenshots/CG-t13g08-tp4-2.png)
+*Figure 2: TP4-2*
