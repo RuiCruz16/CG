6ccea43d88caebbb558704742b02e8d97067e759
Miguel Guerrinha <up202205038@up.pt>
2025-04-03 12:34:57 +0100

    sphere

--- project/MyScene.js
+++ project/MyScene.js
@@ -1,6 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis, CGFtexture, CGFappearance } from "../lib/CGF.js";
 import { MyPlane } from "./MyPlane.js";
-//import { MySphere } from "./MySphere.js";
+import { MySphere } from "./MySphere.js";
 
 /**
  * MyScene
@@ -42,7 +42,7 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this, 20, 1);
     this.plane = new MyPlane(this, 64);
-    //this.sphere = new MySphere(this, 20, 20);
+    this.sphere = new MySphere(this, 50, 50);
   }
   initLights() {
     this.lights[0].setPosition(200, 200, 200, 1);
@@ -103,10 +103,20 @@ export class MyScene extends CGFscene {
 
     this.setDefaultAppearance();
 
+    this.pushMatrix();
     this.scale(400, 1, 400);
     this.rotate(-Math.PI / 2, 1, 0, 0);
     //this.sphere.display();
     this.planeMaterial.apply(); // Apply the texture to the plane!
     this.plane.display();
+    this.popMatrix();
+
+    this.pushMatrix();
+    this.translate(0, 100, 0);
+    this.scale(50, 50, 50);
+    this.sphere.display();
+    this.popMatrix();
+
+
   }
 }
---
+++ project/MySphere.js
@@ -0,0 +1,55 @@
+import { CGFobject } from "../lib/CGF.js";
+
+/**
+ * MySphere
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+
+export class MySphere extends CGFobject {
+    constructor(scene, slices, stacks) {
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        let sliceAngle = 2 * Math.PI / this.slices;
+        let stackAngle = Math.PI / this.stacks;
+
+        for (let i = 0; i <= this.stacks; i++) {
+            let stackHeight = Math.cos(i * stackAngle);
+            let radius = Math.sin(i * stackAngle);
+
+            for (let j = 0; j <= this.slices; j++) {
+                let angle = j * sliceAngle;
+                let x = radius * Math.cos(angle);
+                let y = radius * Math.sin(angle);
+                let z = stackHeight;
+
+                this.vertices.push(x, y, z);
+                this.normals.push(x, y, z);
+            }
+        }
+
+        for (let i = 0; i < this.stacks; i++) {
+            for (let j = 0; j < this.slices; j++) {
+                let first = (i * (this.slices + 1)) + j;
+                let second = first + this.slices + 1;
+
+                this.indices.push(first, second, first + 1);
+                this.indices.push(second, second + 1, first + 1);
+            }
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+    
+}
